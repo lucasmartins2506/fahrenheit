89508ea92f7aa3207f82777ee18e051d9919b3a7
lucasmartins2506 <lucasnalua25@gmail.com>
2019-06-28 21:12:02 -0300

    [feat]:calculadora fahrenheit

---
+++ Fahrenheit/Fahrenheit/Conversor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrenheit
+{
+    class Conversor
+    {
+        //Nomes das variáveis sempre em minúsculos
+        private double tempCelsius;
+        private double tempFahrenheit;
+        public double TempCelsius { get => tempCelsius; set => tempCelsius = value; }
+        public double TempFahrenheit { get => tempFahrenheit; set => tempFahrenheit = value; }
+
+        public double ConverterParaCelsius(){
+            //Método que converte de Fahrenheit para Celsius
+            //Formula matemática para conversão de Fahrenheit pra Celsius
+            double celsius = (TempFahrenheit - 32) / 1.8;
+            return (celsius);
+        }
+        public double ConverterParaFahrenheit()
+        {
+            //Método que converte de Celsius para Fahrenheit
+            //Formula matemática para conversão de Celsius para Fahrenheit
+            double fahrenheit = 1.8 * tempCelsius + 32;
+            return (fahrenheit);
+        }
+    }
+}
---
+++ Fahrenheit/Fahrenheit/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fahrenheit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConverter_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (rdbCelsiuspfahr.Checked)
+                {
+
+                    //Converte de Celsius para Fahrenheit
+                    Conversor conv = new Conversor();
+                    conv.TempCelsius = Double.Parse(txtCelsius.Text);
+                    txtFahrenheit.Text = Convert.ToString(conv.ConverterParaFahrenheit());
+                }
+                else if (rdbFahrenheitpCelsius.Checked)
+                {
+                    //Converte de Fahrenheit para Celsius
+                    Conversor conv = new Conversor();
+                    conv.TempFahrenheit = Double.Parse(txtFahrenheit.Text);
+                    txtCelsius.Text = Convert.ToString(conv.ConverterParaCelsius());
+                }
+                else
+                {
+                    MessageBox.Show("Selecione uma opção");
+                }
+            }catch( FormatException)
+            {
+                MessageBox.Show("Digite um número");
+
+            }
+
+        } 
+
+        private void rdbCelsiuspfahr_CheckedChanged(object sender, EventArgs e)
+        {
+            txtFahrenheit.Enabled = false;
+            txtCelsius.Enabled = true;
+        }
+
+        private void rdbFahrenheitpCelsius_CheckedChanged(object sender, EventArgs e)
+        {
+            txtCelsius.Enabled = false;
+            txtFahrenheit.Enabled = true;
+        }
+    }
+}
